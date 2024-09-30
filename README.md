604a3078d6f28871348e06c9d594a68b3f984253
Jun Jun M. Zaragosa <144114300+jzaragosa06@users.noreply.github.com>
2024-09-30 14:54:55 +0000

    added the evaluation of each models. Separated into functions.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ml-for-forecasting
 This repository checks for non-deep learning machine learning we can use in conjunction with skforecast library to 
 make a time seriesforecasting model.
+results['results_random_search']
--- index.py
+++ index.py
@@ -1,9 +1,14 @@
 
+import pandas as pd
+import numpy as np
+from models.random_forest import *
+from utility.date_functions import *
 
+df = pd.read_csv('./datasets/candy_production.csv', index_col=0, parse_dates=True)
 
-df = pd.read_csv('', index_col=0, parse_dates=True)
 freq = infer_frequency(df)
-results = forecast_and_evaluate_random_forest(df_arg = df, exog=exog)
+exog = create_time_features(df=df, freq=freq)
+results = forecast_and_evaluate_random_forest(df_arg = df, exog=exog, lag_value=15)
 
 
 print(results['mae'])
