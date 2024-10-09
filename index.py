7f565114cc73d21fbf67e160043ed563ba2f5307
Jun Jun M. Zaragosa <144114300+jzaragosa06@users.noreply.github.com>
2024-10-09 11:53:56 +0000

    results

--- index.py
+++ index.py
@@ -1,18 +1,4 @@
 
-# import pandas as pd
-# import numpy as np
-# from models.random_forest import *
-# from utility.date_functions import *
-
-# df = pd.read_csv('./datasets/candy_production.csv', index_col=0, parse_dates=True)
-
-# freq = infer_frequency(df)
-# exog = create_time_features(df=df, freq=freq)
-# results = forecast_and_evaluate_random_forest(df_arg = df, exog=exog, lag_value=15)
-
-
-# print(results['mae'])
-
 import os
 import pandas as pd
 import numpy as np
